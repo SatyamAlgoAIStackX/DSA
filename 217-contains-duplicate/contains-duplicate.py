2adba22807cf0072dee7d4762ecaf80abb6ab7b0
Satyam Kumar <kumarsatyam52923@gmail.com>
2026-05-01 21:50:04 +0530

    Time: 25 ms (15.42%) | Memory: 37.1 MB (6.32%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,5 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # if len(set(nums)) != len(nums):
+        #     return True
+        # return False
         freq_map =  {} # empty dictionary to store freqency
         for num in nums:
             freq_map[num] = freq_map.get(num,0)+1 #python dictionary concept to store freq
